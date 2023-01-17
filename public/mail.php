b1edfe25f7596e4b0b272decb2d8c301bca20c3e
kacungsamsulhadi <adisamsul38.sh@gmail.com>
2023-01-17 19:46:50 +0700

    connect form to mail

---
+++ public/mail.php
@@ -0,0 +1,16 @@
+<?php
+// get data from form
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+
+$to = "";
+$subject = "Mail From Website";
+$txt ="Name = ". $name . "\r\nEmail = " . $email . "\r\nMessage =" . $message;
+$headers = "From: noreply@site.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+// redirect
+header("Location:thankyou.html");
+?>
